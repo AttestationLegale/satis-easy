73a81ec9e68d5cf11409b7928e3d39b120c0ee10
rebolon <richard.tribes@gmail.com>
2015-06-26 15:46:32 +0200

    improve and complete bin shell script

--- bin/start.bat
+++ bin/start.bat
@@ -1,6 +1,36 @@
-@echo off
+@ECHO off
 
-IF [%NPM_PACKAGE_CONFIG_MONGOURL%] NEQ [] SET MONGO_URL=%NPM_PACKAGE_CONFIG_MONGOURL%
-IF [%1] NEQ [] SET MONGO_URL=%1
+ECHO "*** start configuration ***"
 
-meteor --settings settings.json
+IF [%NPM_PACKAGE_CONFIG_PORT%] NEQ [] (
+    ECHO "Port settled to %NPM_PACKAGE_CONFIG_PORT%"
+    SET MONGO_URL=%NPM_PACKAGE_CONFIG_PORT%
+) ELSE (
+    ECHO "No port config found in package.json, Meteor will run on 3000"
+)
+IF [%NPM_PACKAGE_CONFIG_ROOTURL%] NEQ [] (
+    ECHO "ROOT_URL settled to %NPM_PACKAGE_CONFIG_ROOTURL%"
+    SET MONGO_URL=%NPM_PACKAGE_CONFIG_ROOTURL%
+) ELSE (
+    ECHO "No root_url config found in package.json"
+)
+IF [%NPM_PACKAGE_CONFIG_MONGOURL%] NEQ [] (
+    ECHO "MONGO_URL settled to %NPM_PACKAGE_CONFIG_MONGOURL%"
+    SET MONGO_URL=%NPM_PACKAGE_CONFIG_MONGOURL%
+) ELSE (
+    ECHO "No mongo_url config found in package.json, Meteor will use internal mongo"
+)
+IF [%NPM_PACKAGE_CONFIG_MAILURL%] NEQ [] (
+    ECHO "MAIL_URL settled to %NPM_PACKAGE_CONFIG_MAILURL%"
+    SET MAIL_URL=%NPM_PACKAGE_CONFIG_MAILURL%
+) ELSE (
+    ECHO "No mail_url config found in package.json"
+)
+
+ECHO "*** end configuration ***"
+
+IF EXIST %NPM_PACKAGE_CONFIG_SETTINGSFILE% CMD /c "ECHO "Start meteor with settings file %NPM_PACKAGE_CONFIG_SETTINGSFILE%""
+IF EXIST %NPM_PACKAGE_CONFIG_SETTINGSFILE% CMD /c "meteor --settings %NPM_PACKAGE_CONFIG_SETTINGSFILE%"
+
+IF NOT EXIST %NPM_PACKAGE_CONFIG_SETTINGSFILE% CMD /c "ECHO "Start meteor without settings""
+IF NOT EXIST %NPM_PACKAGE_CONFIG_SETTINGSFILE% CMD /c "meteor"
--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,43 @@
 #!/usr/bin/env bash
 
-if [ -z "$npm_package_config_mongourl" ]; then echo "No mongo_url config found in package.json"; else export MONGO_URL=$npm_package_config_mongourl; fi
-if [ -z "$1" ]; then echo "Meteor will use internal mongo"; else export MONGO_URL=$1; fi
+echo "*** start configuration ***"
 
-meteor --settings settings.json
+if [ -z "$npm_package_config_port" ];
+then
+    echo "No port config found in package.json, Meteor will run on 3000";
+else
+    echo "Port settled to $npm_package_config_port"
+    export PORT=$npm_package_config_port;
+fi
+if [ -z "$npm_package_config_rooturl" ];
+then
+    echo "No root_url config found in package.json";
+else
+    echo "ROOT_URL settled to $npm_package_config_rooturl"
+    export ROOT_URL=$npm_package_config_rooturl;
+fi
+if [ -z "$npm_package_config_mongourl" ];
+then
+    echo "No mongo_url config found in package.json, Meteor will use internal mongo";
+else
+    echo "MONGO_URL settled to $npm_package_config_mongourl"
+    export MONGO_URL=$npm_package_config_mongourl;
+fi
+if [ -z "$npm_package_config_mailurl" ];
+then
+    echo "No mail_url config found in package.json";
+else
+    echo "MAIL_URL settled to $npm_package_config_mailurl"
+    export MAIL_URL=$npm_package_config_mailurl;
+fi
+
+echo "*** end configuration ***"
+
+# start with specified
+if [ -z "$npm_package_config_settingsfile" ] || [ -f $npm_package_config_settingsfile ];
+then
+    echo "Start meteor without settings" && meteor;
+else
+    echo "Start meteor with settings file $npm_package_config_settingsfile"
+    meteor --settings $npm_package_config_settingsfile;
+fi
